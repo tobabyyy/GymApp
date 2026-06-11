6b349da90bec55ea4e3dec75d70b73c0a24f71cc
tobabyyy <tobias.hartmann.04@gmail.com>
2026-06-11 19:28:05 +0200

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,4 +0,0 @@
-window.GB_SUPABASE_CONFIG = {
-  url: "https://cuxcgfdstqxhxvcyyqtp.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1eGNnZmRzdHF4aHh2Y3l5cXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NDI1MzksImV4cCI6MjA5NTAxODUzOX0.ihBzgVOglFzcCIkp10U72tGKoBdyKs6FJWmpKdg5o-o"
-};
